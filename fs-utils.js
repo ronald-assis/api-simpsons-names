73b83aa8a8cd34e12c514e55e4a50358a6dff7a5
ronald_assis <ronaldassis71@gmail.com>
2022-03-17 14:53:11 -0300

    feat/fix: create file  fs-utils

---
+++ fs-utils.js
@@ -0,0 +1,15 @@
+const fs = require('fs/promises');
+const { networkInterfaces } = require('os');
+
+const getSimpsons = async () => {
+  const readFile = await fs.readFile('./simpsons.json, utf-8');
+  const fileContent = await JSON.parse(readFile);
+
+  return fileContent;
+}
+
+const setSimpsons = (newSimpson) => {
+  return fs.writeFile('./simpsons.json', JSON.parse(newSimpson));
+}
+
+module.exports = {getSimpsons, setSimpsons}
